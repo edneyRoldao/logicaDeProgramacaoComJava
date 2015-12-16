620ec9a39e6d630e4efed4113e45bf4864998307
edneyroldao <edneyroldao@yahoo.com.br>
2015-12-16 20:07:07 -0200

    mais classes sobre arrays

---
+++ ExerciciosPraticos/src/ordenacao/SegundoExercicoOrdenacao.java
@@ -0,0 +1,24 @@
+package ordenacao;
+
+/**
+ * @author edney.souza
+ * 
+ * Proposta: Construir um metodo que recebe como parametro dois arrays simples com valores inteiros.
+ * 			 Ambos possuem ja estao com seus valores ordenados.
+ * 			
+ * 			 O metodo deve retornar um terceira array e este deve receber os valores dos dois arrays que serao
+ * 			 passados como parametro. Este array deve ser retornado de forma ordenada						
+ *
+ */
+public class SegundoExercicoOrdenacao {
+	
+	
+	
+	public static int[] juntarDoisArrays(int[] a, int[] b) {
+		
+		int[] c = new int[a.length + b.length];
+		
+		return c;
+	}
+
+}
