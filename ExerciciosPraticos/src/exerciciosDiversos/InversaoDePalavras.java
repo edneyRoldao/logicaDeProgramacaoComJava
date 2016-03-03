80e954e236f319521c2cf40f3b5bb8c675275d54
edneyRoldao <edneyroldao@gmail.com>
2016-03-03 15:14:06 -0300

    adicionada classe que verifica palindromos

--- ExerciciosPraticos/src/exerciciosDiversos/InversaoDePalavras.java
+++ ExerciciosPraticos/src/exerciciosDiversos/InversaoDePalavras.java
@@ -1,26 +1,34 @@
 package exerciciosDiversos;
 
+import java.util.Scanner;
+
 /**
  * 
  * @author edneyroldao
  *
- *Vomos implementar mÃ©todos que invertem a ordem das palavras, de tras para frente e vice-versa
+ *Vamos implementar métodos que invertem a ordem das palavras, de tras para frente e vice-versa
  *
  */
 public class InversaoDePalavras {
 	
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		desinverterPalavra("nadine");
+
+		System.out.println("Informa a palavra:");
+		String nome = sc.nextLine();
+
+		desinverterPalavra(nome);
 	}
 	
-	
+	//Este mesmo método pode ser utilizado para verificar se uma palavrar é um palindromo.
 	public static void desinverterPalavra(String palavraInvertida) {
 		
 		String nome = "";
 		
 		for(int i = palavraInvertida.length() -1; i >= 0; i--) {
 			char letra = palavraInvertida.charAt(i);
-			nome += letra;
+			nome = nome +letra;
 		}
 		System.out.println("Palavra: " + nome);
 	}
