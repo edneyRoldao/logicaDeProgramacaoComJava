0d6bc4f086b5c8bf366c1dce6fff1aeb62991d5c
edneyRoldao <edneyroldao@gmail.com>
2016-03-10 16:05:43 -0300

    added first example about reflections api from book casa do codigo

---
+++ ExemplosExerciciosLivrosCasaDoCodigo/src/reflections/firstExample/Produto.java
@@ -0,0 +1,61 @@
+package reflections.firstExample;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	//Attributes
+	private String name;
+	private String category;
+	private Double price;
+	private String description;
+
+	//Constructors
+	public Produto(String name, String category, Double price, String description) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.description = description;
+	}
+
+	public Produto() {
+
+	}
+
+	//Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ ExemplosExerciciosLivrosCasaDoCodigo/src/reflections/firstExample/TestGenerateMethodsMap.java
@@ -0,0 +1,19 @@
+package reflections.firstExample;
+
+import java.util.Map;
+
+public class TestGenerateMethodsMap {
+	
+	public static void main(String[] args) {
+		
+		Produto computer = new Produto("Sony vaio", "leptops", 3.200, "Core i7");
+		
+		Map<String, Object> props = GeradorDeMapas.gerarMapaMetodos(computer);
+		
+		for(String prop : props.keySet()) {
+			System.out.println(prop + " = " + props.get(prop));
+		}
+ 		
+	}
+
+}
