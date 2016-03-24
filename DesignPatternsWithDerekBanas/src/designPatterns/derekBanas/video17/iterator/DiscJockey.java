77921c753a2a3550eb7a36da2204204e4f0e43e1
edneyRoldao <edneyroldao@gmail.com>
2016-03-24 00:34:10 -0300

    was added some classes again for Iterator Pattern

--- DesignPatternsWithDerekBanas/src/designPatterns/derekBanas/video17/iterator/DiscJockey.java
+++ DesignPatternsWithDerekBanas/src/designPatterns/derekBanas/video17/iterator/DiscJockey.java
@@ -1,8 +1,61 @@
 package designPatterns.derekBanas.video17.iterator;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+/**
+ * Esta classe serve para explicar porque usar o Pattern Iterator.
+ * Veja que nessa classe tivemos que duplicar o codigo para fazer o mesmo tipo de iteracao pois cada colecao de musica e de um tipo diferente.
+ * Um e array simple, hashTable e ArrayList
+ * 
+ * @author EdneyRoldao
+ *
+ */
 public class DiscJockey {
 
-	// 8:00
-	// https://www.youtube.com/watch?v=VKIzUuMdmag&index=17&list=PLF206E906175C7E07
+	MusicasAnos70 musicasAnos70;
+	MusicasAnos80 musicasAnos80;
+	MusicasAnos90 musicasAnos90;
+	
+	public DiscJockey(MusicasAnos70 musicasAnos70, MusicasAnos80 musicasAnos80, MusicasAnos90 musicasAnos90) {
+		this.musicasAnos70 = musicasAnos70;
+		this.musicasAnos80 = musicasAnos80;
+		this.musicasAnos90 = musicasAnos90;
+	}
+	
+	// este metodo apresenta codigo duplicado. Isso nao e uma boa pratica
+	public void showTheSongs() {
+
+		@SuppressWarnings("rawtypes")
+		ArrayList al70sSongs = musicasAnos70.getBestSongs();
+		System.out.println("Songs of the 70s\n");
+		for(int i = 0; i < al70sSongs.size();i++) {
+			SongInformation bestSong = (SongInformation) al70sSongs.get(i);
+			System.out.println(bestSong.getBandName());
+			System.out.println(bestSong.getSongName());
+			System.out.println(bestSong.getYearReleased());
+		}
+
+		SongInformation[] array80sSongs = musicasAnos80.getBestSongs();
+		System.out.println("Songs of the 80s\n");
+		for(int i = 0; i < array80sSongs.length;i++) {
+			SongInformation bestSong = array80sSongs[i];
+			System.out.println(bestSong.getBandName());
+			System.out.println(bestSong.getSongName());
+			System.out.println(bestSong.getYearReleased());
+		}
+		
+		Hashtable<Integer, SongInformation> ht90sSongs = musicasAnos90.getBestSongs();
+		System.out.println("Songs of the 90s\n");
+
+		for(Enumeration<Integer> e = ht90sSongs.keys(); e.hasMoreElements();) {
+			SongInformation bestSong = ht90sSongs.get(e.nextElement());
+			System.out.println(bestSong.getBandName());
+			System.out.println(bestSong.getSongName());
+			System.out.println(bestSong.getYearReleased());
+		}
+		
+	}
 	
 }
