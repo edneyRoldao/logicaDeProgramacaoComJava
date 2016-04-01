4f29fd57dfb561afb2d09500672e61181a343111
edneyRoldao <edneyroldao@gmail.com>
2016-04-01 17:33:28 -0300

    more about strategy pattern

--- ExerciciosPraticosDesignPatterns/src/pattern/strategy/exemploComPattern/Veiculo.java
+++ ExerciciosPraticosDesignPatterns/src/pattern/strategy/exemploComPattern/Veiculo.java
@@ -1,5 +1,25 @@
 package pattern.strategy.exemploComPattern;
 
-public class Veiculo {
+public abstract class Veiculo {
+
+	private String nome;
+	private String marca;
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
 
 }
